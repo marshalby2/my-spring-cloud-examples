eebe280e59cad6c9060708c953a9dbcaa1f79ea2
marshalby2 <marshalby2@gmail.com>
2020-10-13 19:49:41 +0800

    :satisfied: eureka

--- eureka-demo/eureka-client-hello-service/src/main/java/com/my/eurekaclienthelloservice/controller/HelloController.java
+++ eureka-demo/eureka-client-hello-service/src/main/java/com/my/eurekaclienthelloservice/controller/HelloController.java
@@ -1,9 +1,21 @@
 package com.my.eurekaclienthelloservice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @Description TODO
  * @Author marshal
  * @Date 12/10/20 7:36 PM
  */
+@RestController
+@RequestMapping("/hello")
 public class HelloController {
+
+    @GetMapping("/greet")
+    public String greet() {
+        return "hello";
+    }
 }
---
+++ eureka-demo/eureka-client-hello-service/src/test/java/com/my/eurekaclienthelloservice/EurekaClientHelloServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.eurekaclienthelloservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientHelloServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
